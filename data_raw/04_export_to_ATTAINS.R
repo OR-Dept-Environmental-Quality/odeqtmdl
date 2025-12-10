21398c7c8b124b2010d99fd3b3abdf7f84fcf041
Ryan Michie <ryan.michie@deq.oregon.gov>
2025-12-09 22:19:36 -0800

    Update 04_export_to_ATTAINS.R

--- data_raw/04_export_to_ATTAINS.R
+++ data_raw/04_export_to_ATTAINS.R
@@ -1,6 +1,13 @@
 library(odeqtmdl)
+library(readxl)
+
+# Read paths
+paths <- readxl::read_excel(path = "data_raw/project_paths.xlsx",
+                            sheet = "paths" , col_names = TRUE,
+                            na = c("", "NA"),
+                            col_types = c("text", "text"))
 
 action_ids <- "OR_TMDL_20230915"
-out_dir <- "C:/Users/rmichie/OneDrive - Oregon/Projects/TMDL_ATTAINS_Project/ATTAINS_uploads"
+out_dir <- file.path(paths$attains_project_path[1], "ATTAINS_uploads")
 
 export_status <- tmdl_export_attains(out_dir = out_dir, action_ids = action_ids)
